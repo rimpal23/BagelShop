4fd23f405ac8db53bfb69bae6bd436df8d87793e
rimpal23 <124216677+rimpal23@users.noreply.github.com>
2023-02-02 11:55:55 -0500

    Update AddressView.java

--- AdrressBook/src/AddressBook/AddressView.java
+++ AdrressBook/src/AddressBook/AddressView.java
@@ -1,24 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package AddressBook;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author 
- */
+
 public class AddressView {
 
 	private AddressController controller;
-	/**
-	 * 
-	 * @param args
-	 */
+	
         
         public AddressView() {
 		controller=new AddressController();
