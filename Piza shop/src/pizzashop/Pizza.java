875059eebc88965b5d0b335c4bda0eca34151421
rimpal23 <124216677+rimpal23@users.noreply.github.com>
2023-02-02 11:51:06 -0500

    Update Pizza.java

--- Piza shop/src/pizzashop/Pizza.java
+++ Piza shop/src/pizzashop/Pizza.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pizzashop;
 
 import java.util.ArrayList;
 
 /**
  * A class that represents a basic pizza
- * @author dancye
  */
 public class Pizza 
 {
